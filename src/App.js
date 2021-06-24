c1465faf9be75bda6ec2e6d273cc7cc3c6f39600
hyunwoo045 <hyunwoo045@gmail.com>
2021-06-25 00:30:06 +0900

    feat: add animation appears from left

--- src/App.js
+++ src/App.js
@@ -6,26 +6,59 @@ const data = [{ title: "Group 1" }, { title: "Group 2" }, { title: "Group 3" }];
 
 function App() {
   const [curIdx, setIdx] = useState(0);
+  const [curDir, setDir] = useState("left");
   const transitions = useTransition(curIdx, {
     from: { opacity: 0, transform: "translate3d(100%, 0, 0)" },
     enter: { opacity: 1, transform: "translate3d(0%, 0, 0)" },
     leave: { opacity: 0, transform: "translate3d(-50%, 0, 0)" },
   });
+  const transRight = useTransition(curIdx, {
+    from: { opacity: 0, transform: "translate3d(-100%, 0, 0)" },
+    enter: { opacity: 1, transform: "translate3d(0%, 0, 0)" },
+    leave: { opacity: 0, transform: "translate3d(50%, 0, 0)" },
+  });
   return (
     <div className="app">
-      {transitions((style, i) => {
-        return (
-          <animated.div
-            className="box"
-            style={style}
-            onClick={() => {
-              setIdx((curIdx + 1) % 3);
-            }}
-          >
-            {data[i].title}
-          </animated.div>
-        );
-      })}
+      <div id="hello">
+        <button
+          onClick={() => {
+            if (curIdx === 0) setIdx(2);
+            else setIdx((curIdx - 1) % 3);
+            setDir("right");
+          }}
+        >
+          헬 로
+        </button>
+      </div>
+      {curDir === "left"
+        ? transitions((style, i) => {
+            return (
+              <animated.div
+                className="box"
+                style={style}
+                onClick={() => {
+                  setIdx((curIdx + 1) % 3);
+                  setDir("left");
+                }}
+              >
+                {data[i].title}
+              </animated.div>
+            );
+          })
+        : transRight((style, i) => {
+            return (
+              <animated.div
+                className="box"
+                style={style}
+                onClick={() => {
+                  setIdx((curIdx + 1) % 3);
+                  setDir("left");
+                }}
+              >
+                {data[i].title}
+              </animated.div>
+            );
+          })}
     </div>
   );
 }
